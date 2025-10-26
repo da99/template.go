7a1442e32998059d252a623067ac392100380a3e
da99 <1551848+da99@users.noreply.github.com>
2025-10-26 12:39:03 -0500

    Development checkpoint.

---
+++ bin/main.go
@@ -0,0 +1,32 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/da99/files.go/files"
+	"github.com/da99/cli.go/args"
+)
+
+func main() {
+
+	if args.IsMatch("-h", 0) {
+		fmt.Println("  -h|help|--help   -- This message.")
+		fmt.Println("  compile (dir)    -- Compile all files or just the `dir`.")
+		return
+	}
+
+	if args.IsMatch("compile", 0) {
+		dirs, err := files.List_Shallow_Dirs("public/section")
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		for _, d := range dirs {
+			fmt.Println(d)
+		}
+		return
+	}
+
+	args.Fail()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/da99/template.go
+
+go 1.25.2
